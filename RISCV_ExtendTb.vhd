eea5ef1d996b886e277aeb43977a300d64ce01cb
starfruit-zl <zacharylord.work@gmail.com>
2025-03-23 00:48:43 -0400

    Modifying Testbench for Extend

--- RISCV_ExtendTb.vhd
+++ RISCV_ExtendTb.vhd
@@ -1,43 +1,76 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.numeric_std.ALL; --changed the library to the numeric standard.
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
-entity extend_unit_tb is
-end extend_unit_tb;
+entity tb is
 
-architecture testbench of extend_unit_tb is
-    signal imm_in      : std_logic_vector(31 downto 7);
-    signal extend_ctrl : std_logic_vector(2 downto 0);
-    signal imm_out     : std_logic_vector(31 downto 0);
+end tb;
 
-    -- Instantiate the Extend Unit module, !!may need to move to 
-    component extend_unit
-        port (
-            imm_in      : in std_logic_vector(31 downto 7);
-            extend_ctrl : in std_logic_vector(2 downto 0);
-            imm_out     : out std_logic_vector(31 downto 0)
-        );
-    end component;
 
-begin
-    dut: extend_unit port map (
-        imm_in      => imm_in,
-        extend_ctrl => extend_ctrl,
-        imm_out     => imm_out
-    );
+architecture test of tb is
 
-    process
-    begin
-        
-        -- Test cases (imm_in is only bits [31:7] of the instruction)
-        imm_in <= '0'&x"000FFF"; extend_ctrl <= "000"; wait for 10 ns;
-        imm_in <= '0'&x"7FFFFF"; extend_ctrl <= "000"; wait for 10 ns; -- Negative case (MSB = 1)
-        imm_in <= '0'&x"000FFF"; extend_ctrl <= "001"; wait for 10 ns;
-        imm_in <= '0'&x"000FFF"; extend_ctrl <= "010"; wait for 10 ns;
-        imm_in <= '0'&x"000FFF"; extend_ctrl <= "011"; wait for 10 ns;
-        imm_in <= '0'&x"000FFF"; extend_ctrl <= "100"; wait for 10 ns;
-        
-        report "Test completed.";
-        wait;
-    end process;
-end testbench;
+	constant I_Type : std_logic_vector(2 downto 0) := "000";
+	constant S_Type : std_logic_vector(2 downto 0) := "001";
+	constant B_Type : std_logic_vector(2 downto 0) := "010";
+	constant U_Type : std_logic_vector(2 downto 0) := "011";
+	constant J_Type : std_logic_vector(2 downto 0) := "100";
+
+	signal imm : std_logic_vector(31 downto 7) := (others => '0');
+	signal immSrc : std_logic_vector(2 downto 0) := (others => '0');
+	signal immExt : std_logic_vector(31 downto 0) := (others => '0');
+	
+
+begin 
+
+	Extend : entity work.Extend(behave)
+		port map
+		(
+			imm => imm,
+			immSrc => immSrc,
+			immExt => immExt
+		);
+		
+	process
+	begin
+	
+		--  Case 1: I_Type:  ADDI x5, x1, -10 --
+		immSrc <= I_Type;
+		imm <= "111111110110" & "0000100000101";
+		wait for 10 ns;
+		
+		assert to_integer(signed(immExt)) = -10 report "Case 1: Failed";
+		
+		-- Case 2: S_Type:  SW x6, -52 --
+		immSrc <= S_Type;
+		imm <= "1111110" & "00110" & "00010" & "010" &  "01100";
+		wait for 10 ns;
+		
+		assert to_integer(signed(immExt)) = -52 report "Case 2: Failed";
+		
+		-- Case 3: J_Type:  JAL x1, 1024 --
+		immSrc <= J_Type;
+		imm <= '0' & "1000000000" & '0' & "00000000" & "00001";
+		wait for 10 ns;
+		
+		assert to_integer(signed(immExt)) = 1024 report "Case 3: Failed";
+		
+		-- Case 4: B_Type:  BEQ x3, x4, -16 --
+		immSrc <= B_Type;
+		imm <= '1' & "111111" & "00100" &  "00011" & "000" & "1000" & '1';
+		wait for 10 ns;
+		
+		assert to_integer(signed(immExt)) = -16 report "Case 4: Failed";
+		
+		-- Case 5: U_Type:  LUI x7, 0x12345 --
+		immSrc <= U_Type;
+		imm <= x"12345" & "00111";
+		wait for 10 ns;
+		
+		assert immExt = x"12345000" report "Case 5: Failed";
+		
+		
+		report "Test Bench Complete";
+		wait;
+	
+	end process;	
+end test;
