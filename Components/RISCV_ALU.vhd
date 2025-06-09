18734859b00dc2470d5c5871b385e0f82997c6a5
starfruit.zl <zacharylord.work@gmail.com>
2025-06-08 23:05:12 -0400

    updating alu

--- Components/RISCV_ALU.vhd
+++ Components/RISCV_ALU.vhd
@@ -22,48 +22,39 @@ port(
 end entity;
 
 architecture behave of RISCV_ALU is
-	signal temp_result : std_logic(32 downto 0);
+	signal temp_result : std_logic_vector(32 downto 0);
 	signal V : std_logic;
 begin
 
 	process (ALU_ctrl, A, B) is
-	variable inputA : signed(31 downto 0);
-	variable inputB : signed(31 downto 0);
+	variable inputA : signed(32 downto 0);
+	variable inputB : signed(32 downto 0);
 	variable shiftNum : integer;
-	variable output : signed(31 downto 0);
-
 	begin
-		inputA := signed(A);
-		inputB := signed(B);	
-		output := (others => '0');
+		inputA := signed('0' & A);
+		inputB := signed('0' & B);	
 		shiftNum := to_integer(unsigned(B));
-		carry <= '0';
-		overflow <= '0';
-		negative <= '0';
-		zero <= '0';		
 
 
 		case ALU_ctrl is
 			when "0000" => --add
-				temp_result <= std_logic_vector(inputA + inputB);
+				temp_result <= std_logic_vector(signed('0' & A) + signed('0' & B));
 
 			when "0001" | "0111" | "1000" =>  --sub, slt, sltu. Improvement, re-use the sub hardware.
-				temp_result <= std_logic_vector(inputA - inputB);
+				temp_result <= std_logic_vector(signed('0' & A) + ('0' & (not signed(B) + 1)));
 
 			when "0010" =>  --and (if it turns out that flags are logical, stop setting output will disallow check).
-				temp_result <= A and B;
+				temp_result(31 downto 0) <= A and B;
 
 			when "0011" =>  --or
-				temp_result <= A or B;
+				temp_result(31 downto 0) <= A or B;
 
 			when "0100" =>  --xor
-				temp_result <= A xor B;
+				temp_result(31 downto 0) <= A xor B;
 
 			when "0101" =>  --sll --add #B 0's to left of A (I am aware of built-in function, was just curious).
-				if shiftNum = 0 then
-				temp_result <= A;
+				if shiftNum /= 0 then
 
-				else
 				for i in 31 downto shiftNum loop
 				inputA(i) := inputA(i-shiftNum);
 				end loop;
@@ -71,16 +62,14 @@ begin
 				for i in shiftNum-1 downto 0 loop
 				inputA(i) := '0';
 				end loop;
+
+				end if;
 			
 				temp_result <= std_logic_vector(inputA);
-				end if;
 
 			when "0110" => --srl --add 0 to right
 
-				if shiftNum = 0 then
-				temp_result <= A;
-
-				else
+				if shiftNum /= 0 then
 				for i in 0 to 31-shiftNum loop --opposite direction as to not attempt to grab undefined values.
 				inputA(i) := inputA(i+shiftNum);
 				end loop;
@@ -89,12 +78,25 @@ begin
 				inputA(i) := '0';
 				end loop;
 
-				temp_result <= std_logic_vector(inputA);
 				end if;
 
+				temp_result <= std_logic_vector(inputA);
+
 			when "1001" =>  --sra:
+				if shiftNum /= 0 then
 
+				for i in 0 to 31-shiftNum loop --opposite direction as to not attempt to grab undefined values.
+				inputA(i) := inputA(i+shiftNum);
+				end loop;
 
+				for i in 32-shiftNum to 31 loop
+				inputA(i) := inputA(31-shiftNum);
+				end loop;
+
+				end if;
+
+				temp_result <= std_logic_vector(inputA);
+				
 			when others =>
 				temp_result <= (others => '0');
 		end case;
@@ -113,9 +115,9 @@ begin
 	end process;
 	
 	with ALU_ctrl select
-	result <= (x"0000000" & "000" & (temp_result(31) xor V)) when "0111",
-			  else (x"0000000" & "000" & not temp_result(32)) when "1001",
-			  else temp_result(31 downto 0);
+	result <= (x"0000000" & "000" & (temp_result(31) xor V)) when "0111", --slt
+			  (x"0000000" & "000" & not temp_result(32)) when "1000", --sltu
+			   temp_result(31 downto 0) when others;
 	
 	overflow <= V when alu_ctrl = "0000" or alu_ctrl = "0001" else '0';
 	
